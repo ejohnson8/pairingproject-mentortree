8321abb1c113cdb1fa1d2ad768b28139594aa2a6
Elizabeth Johnson <ejohnson@solstice.com>
2018-08-28 11:53:46 -0500

    new mentor tree service

---
+++ src/main/java/com/mentortree/mentortreeservice/MentorTreeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mentortree.mentortreeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorTreeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorTreeServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mentortree/mentortreeservice/model/MentorTree.java
@@ -0,0 +1,31 @@
+package com.mentortree.mentortreeservice.model;
+
+public class MentorTree {
+    private String employee;
+    private String mentor;
+    private String treeLead;
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getMentor() {
+        return mentor;
+    }
+
+    public void setMentor(String mentor) {
+        this.mentor = mentor;
+    }
+
+    public String getTreeLead() {
+        return treeLead;
+    }
+
+    public void setTreeLead(String treeLead) {
+        this.treeLead = treeLead;
+    }
+}
