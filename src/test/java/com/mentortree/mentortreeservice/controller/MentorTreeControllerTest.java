1466d191f97f8a00df789c87e867dbd2d22c40cb
Elizabeth Johnson <ejohnson@solstice.com>
2018-09-04 08:22:49 -0500

    test update

--- src/test/java/com/mentortree/mentortreeservice/controller/MentorTreeControllerTest.java
+++ src/test/java/com/mentortree/mentortreeservice/controller/MentorTreeControllerTest.java
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@Ignore
+
 public class MentorTreeControllerTest {
 
     MentorTreeController mentorTreeController;
